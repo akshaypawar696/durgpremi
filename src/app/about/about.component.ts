fd436c126e76cbbd9b1ff11c19c528de234ba7e7
akshay.pawar696@gmail.com <akshay.pawar696@gmail.com>
2020-07-20 23:03:17 +0530

    bhavana

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  numberArray = [
+    {'id':0,'name':'User count','count':100},
+    {'id':1,'name':'Post count','count':200},
+    {'id':2,'name':'Blog count','count':300},
+    {'id':3,'name':'Visitor','count':400},
+    ];
+
   constructor() { }
 
   ngOnInit() {
     console.log('Inside about');
+    display();
   }
 
+  
+}
+function display(){
+  console.log('you are in fun');
 }
