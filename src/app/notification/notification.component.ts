4f62b5e7c7024711c54f117a8f64d026f293592a
akshay.pawar696@gmail.com <akshay.pawar696@gmail.com>
2020-07-27 22:09:18 +0530

    Atul27-07

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -8,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class NotificationComponent implements OnInit {
 
   constructor() { }
+  listArray = [
+    {'id':0,'name':'Raigad'},
+    {'id':1,'name':'Rajgad'},
+    {'id':2,'name':'Sudhagad'},
+    {'id':3,'name':'Sinhagad'},
+    {'id':4,'name':'Lohgad'},
+    {'id':5,'name':'Visapur'},
+    {'id':6,'name':'Torna'},
+    {'id':7,'name':'Purandar'},
+    {'id':8,'name':'Tung'},
+    {'id':9,'name':'Kalsubai'},
+    ];
 
+    trekInfo=[
+      {'title':'Trek organization name : ','name':'Durgpremi trekker'},
+      {'title':'Date : ','name':'28/07/2020'},
+      {'title':'Start point : ','name':'Baner'},
+      {'title':'Cost Per Person : ','name':'1000'},
+      {'title':'Other Facilities : ','name':'Tea,Breakfast,Lunch'},
+      {'title':'No. Of People : ','name':'20'}
+    ]
   ngOnInit() {
-    document.body.style.backgroundImage = "url(assets/download.jpeg)";
+    
   }
 
 }
